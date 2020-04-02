de6765d37dad2e068d2597dda3b672b3b2cb17aa
yomna <yomna.eldawy@bosta.co>
2020-04-02 13:42:10 +0200

    first commit

---
+++ warp.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def warp_inv (im, H):
+    imOut = np.zeros((im.shape[0], 2 * im.shape[1], im.shape[2])).astype(im.dtype)
+    imShape = imOut.shape
+    for i in range(0, imShape[0]):
+        for j in range(0, imShape[1]):
+            dest_pt = np.array([i,j,1])
+            src_pt = np.dot(np.linalg.inv(H), dest_pt)
+            src_pt /= src_pt[2]
+            src_i = int(src_pt[0])
+            src_j = int(src_pt[1])
+            try:
+                imOut[i][j] = im[src_i][src_j]
+            except:
+                None
+    print (imOut.shape)
+    return imOut
